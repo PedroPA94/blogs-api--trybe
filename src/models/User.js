e447ef674ffca0ed89e6737f5c091a760db1ba4d
Pedro Papini <ppapinia@gmail.com>
2022-10-25 16:33:11 -0300

    Resolve requisito 2 - Cria model User

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users'
+  })
+
+  return User;
+};
+
+module.exports = UserModel;
