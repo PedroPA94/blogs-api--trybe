75a2496159159c4fbcd52b87f913a460fa011a99
Pedro Papini <ppapinia@gmail.com>
2022-12-01 20:26:19 -0300

    Adiciona documentação da api pelo swagger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const { authRouter, userRouter, categoryRouter, postRouter } = require('./routers');
+const swaggerDocs = require('./swagger.json');
 require('express-async-errors');
 
 const app = express();
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use('/login', authRouter);
 app.use('/user', userRouter);
